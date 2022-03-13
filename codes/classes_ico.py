e28b94aa121c55ada18dfd76502f8b32473b57f0
mlenglet <76101249+mlenglet@users.noreply.github.com>
2022-03-13 16:17:30 +0100

    fonction list_cosutmers finie

--- codes/classes_ico.py
+++ codes/classes_ico.py
@@ -4,16 +4,21 @@ class Vehicle:
         self.weight = weight
         self.cost_km = cost_km
 
+    def __str__(self):
+        return f'Vehicle of volume {self.volume}, weight {self.weight}'
 
 class Customer:
-    def __init__(self, id, latitude, longitude, time_window, request_volume, request_weight, time_service):
-        self.id = id
+    def __init__(self, identifier, latitude, longitude, time_window, request_volume, request_weight, time_service):
+        self.id = identifier
         self.latitude = latitude
         self.longitude = longitude
         self.time_window = time_window
         self.request_volume = request_volume
         self.request_weight = request_weight
         self.time_service = time_service
+        
+    def __str__(self):
+        return f'This customer\'s id is {self.id}.'
 
 
 class VRPTW:
@@ -25,6 +30,9 @@ class VRPTW:
         self.distances = distances
         self.time_matrix = time_matrix
         self.vehicle = vehicle
+        
+    def __str__(self):
+        return 'Here are the customers : {self.costumers}'
         
         
         
