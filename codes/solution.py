5f24e385517024735f62f89148a8fd219305e850
nicoacevedor <nacevedo@dim.uchile.cl>
2022-03-15 18:33:20 +0100

    cost function added

--- codes/solution.py
+++ codes/solution.py
@@ -1,13 +1,6 @@
-import numpy as np
-import pandas as pd
-import os
-
-from classes_ico import Vehicle, Customer, VRPTW
-from processing import *
-
 def sol_to_list_routes(sol):
-    indexes = [i for i,x in enumerate(sol) if x == 0]
-    liste_divided = [sol[indexes[i]:indexes[i+1]]+[0] for i in range(len(indexes)-1)]
+    indexes = [i for i,x in enumerate(liste) if x == 0]
+    liste_divided = [liste[indexes[i]:indexes[i+1]]+[0] for i in range(len(indexes)-1)]
     return liste_divided
     
 
@@ -15,12 +8,10 @@ def solution_checker(vrptw, sol):
     nb_cust = len(vrptw.customers) # Number of customers (depot included)
     # If all customers are not visited, return False
     if set(sol)!= set(range(nb_cust)):
-        print("Not a solution : all customers are not visited.")
         return False
     # If some nodes (customers) are visited more than once (except for the depot), return False
     nb_depot = sol.count(0)
     if len(sol) != nb_depot+nb_cust-1:
-        print("Not a solution : some customers are visited several times.")
         return False
     vehicle = vrptw.vehicle
     volume, weight, cost_km = vehicle.volume, vehicle.weight, vehicle.cost_km 
@@ -35,7 +26,6 @@ def solution_checker(vrptw, sol):
             volume_cust += cust.request_volume
         # If the weight (or volume) capacity of the vehicle is < to the total weight asked by customers, return False
         if weight < weight_cust or volume < volume_cust :
-            print("Not a solution : the weight (or volume) capacity of the vehicle is < to the total weight asked by customers")
             return False
         for index,identifier in enumerate(route):
             cust = [customer for customer in costumers if customer.id == identifier][0]
@@ -43,32 +33,38 @@ def solution_checker(vrptw, sol):
             time_delivery += time_matrix[cust.code_customer,cust_plus_1.code_customer]
             # If the vehicle gets there befor the beginning of the customer's time window, return False
             if time_delivery<cust_plus_1.time_window[0]:
-                print("Not a solution : some time windows are not respected")
                 return False
             time_delivery += cust_plus_1.time_service
             # If the end of the delivery is after the end of the customer's time window, return False
             if time_delivery>cust_plus_1.time_window[1]:
-                print("Not a solution : some time windows are not respected")
                 return False
     return True
-
-
-
-
     
-# TEST
-#vrptw_test = create_vrptw(CUSTOMER_DIR, DEPOTS_DIR, VEHICLES_DIR, DEPOTS_DISTANCES_DIR, CUSTOMER_DISTANCES_DIR, route_id=2946091, MODE_VEHICLE="mean", vehicle_nb=None)
-#print(vrptw_test.customers)
-#sol = [range(len(vrptw_test.customers)+1)]+[0]
-#solution_checker(vrptw_test, sol)
-
     
-
-'''
-def cost(vrptw, sol):
-    w = 1000
-    nb_vehicle = 
-'''
+# cost
+# returns the total cost of the solution given for the problem given
+# omega is the weight of each vehicle, 1000 by default
+def cost(vrptw, sol, omega=1000):
+    # data retrieval
+    nb_vehicle = sol.count(0)-1
+    distance_matrix = vrptw.distances
+    cost_km = vrptw.vehicle.cost_km
+    customers = vrptw.customers
+    
+    # solution given -> list of routes
+    sol_list = sol_to_list_routes(sol)
+    
+    # sum of the distance of each route
+    route_length = 0
+    for route in sol_list:
+        for i in range(len(route)-1):
+            route_length += distance_matrix[route[i]][route[i+1]]
+    
+    # total cost calculation
+    total_cost = omega*nb_vehicle + cost_km*route_length
+    print('Solution:', sol_list)
+    print('Total cost of solution:', total_cost)
+    return total_cost
 
 
 '''
