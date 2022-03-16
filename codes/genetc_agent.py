ca42af08f87b7dc1cbf7ef0ebaf63d00254cdd47
simpoo2 <46756106+simpoo2@users.noreply.github.com>
2022-03-16 11:57:29 +0100

    Update genetc_agent.py
    Code d'algorithme génétique.
    À réaliser:
    1. la fonction de calculation de fitness
    2. la fonction qui calcule la distance entre deux positions
    3. la corps de program

--- codes/genetc_agent.py
+++ codes/genetc_agent.py
@@ -1,14 +1,34 @@
 from ast import In
 from email.mime import application
+from logging import critical
 import random
-from random import randrange
+from random import randrange, sample
+from statistics import median
 
-class Problem_Genetic(Object):
+
+
+class client():
+    def __init__(self,id,weight,volumn,time_window,vehicles,possition,service_time):
+        self.id=id
+        self.weight=weight
+        self.volumn=volumn
+        self.time_window=time_window
+        self.vehicles=vehicles
+        self.possition=possition
+        self.service_time=service_time
+
+class vehicle():
+    def __init__(self,id,cap_weight,cap_volumn) :
+        self.id=id
+        self.cap_weight=cap_weight
+        self.cap_volumn=cap_volumn
+        
+
+class Problem_Genetic():
     
-    def __init__(self,genes,individuals_length,decode,fitness):
+    def __init__(self,genes,individuals_length,fitness):
         self.genes=genes
         self.individuals_length=individuals_length
-        self.decode=decode
         self.fitness=fitness
         
     def mutation(self,chromosome,prob):
@@ -62,7 +82,7 @@ class Problem_Genetic(Object):
         
         return  process_gen_repeated(child1, child2)
 
-def fitnessVRP(chromosome):
+
 #   Contraintes-faut-il-repecter pour les chromosomes:
 #   Il faut qu'un chromosome réaliser tous les divrasons. Sinon il aura une fitness négatiff
 #   1 - respect du time window
@@ -71,103 +91,9 @@ def fitnessVRP(chromosome):
 #   4 - chaque customer doit etre visité une fois
 #   5 - 1 route = 1 camion
 
-#   data structure correspondant:
-#   chromosome: [0,5,12231,31,45567,1,3456,67689,0], len(chromosome)≈num_client=573 
-#   question sur ce probleme: Parce que le nombre de camion n'est pas fixé entre les chromosomes, est-ce que le crossover est utile? Si le modele ne converge pas ou il converge très doucement?
 
-#对计算能力的质疑...
-#1. time window:
-#   计算当前时间，如果超出服务时间，则该基因死亡（或者削弱？）是否进行下一家的配送？完整配送所有客户是不是必须的？
-#   如果完整配送是必须的：则基因死亡
-#   如果完整配送不是必须的，则基因收到惩罚：fitness收到下降效果
-
-#3. Un route, un camion:
-#   针对该contrainte的两种思路：排除法vs生成法
-#   排除法：先生成chromo,在fitness计算部分 kill掉不符合contrainte的chromos，
-#   优点：生成代码简洁经典。缺点：时间成本高，kill()函数还没想好怎么写:(soit 使用交集，时间复杂度取决于每个客户链接的route)
-#   生成法：在生成chromos阶段就使用该contraint生成
-#   优点：在fitness阶段无需针对此contrainte对chromosome处理
-#   缺点：生成chromosome的方法出现限制，与GA的基本观念有出入，而且目前还没有怎么生成的想法
-#   结论：综合分析结果，采用经典排除法 
-#   
-    def intersection(camion_list1,camion_list2):
-
-        intersection_list=[]
-        if len(camion_list1)<len(camion_list2):
-            for i in camion_list1:
-                if i in camion_list2:
-                    intersection_list.append(i)
-        else:
-            for i in camion_list2:
-                if i in camion_list1:
-                    intersection_list.append(i)   
-
-        return intersection_list    
-
-#   prise en main de contrainte 4: chaque customer doit etre visité une fois
-    def contrainte_4(chromosome):
-
-        client_delivré=set(chromosome)
-        if not len(client_delivré)==574: # len(df2['CUSTOMER_CODE'].unique())=573, on prise en compte de le point de départ
-            return -1
-    
-        if not len(chromosome)-chromosome.count(0)==573:
-            return -1
-        
-#   prise en main des contraintes 1,2,3,5:
-    def contraintes_1235(chromosome):
-
-        taux_penalité_1=0.2
-        taux_penalité_2=0.2
-        taux_penalité_3=0.2
-        taux_penalité_5=0.2
-
-        for i in range(len(chromosome)):
-            # contrainte 5
-            # on a besion d'un chromosome qui satisfait tout les contrainte en même temps. Alors concernat la contrainte de capacité, il faut fixer le camion utilisé dans une route d'abord
-            # problem reste: si il ya plusieur camion dans la liste camion_partagé à la fin d"une route?
-            if chromosome[i] == 0:
-                if not i ==len(chromosome)-1:
-                    camion_partagé=chromosome[i+1].camion_list #chromosome_test[i+1].camion_list ça veut dire la liste de camion qui peut passer le client chromosome[i]
-                else:
-                    continue
-            else:
-                camion_partagé=intersection(camion_partagé,chromosome[i].camion_list)
-            if len(camion_partagé)==0:
-                return -1
-
-        if len(camion_partagé)==1: #cas idéal
-            camion_choisi=camion_partagé[0]
-
-        else:
-            return 0
-
-            # contrainte 1
-            
-#2. contrainte 2,3
-#   for i in range(head,end):
-#       capacité.kg-=client[i].weight
-#       capacité.m3-=client[i].volumn
-#       if capacité.kg<0 or capacité.m3:
-#           kill chromosome
-
-
-#我们对于crossover的chromosome 作出限定：车辆使用数量 
-
-    contrainte_4(chromosome)
-    contraintes_1235(chromosome)
-
-    def TripDistance(chromosome):
-        trip_distance =0
-        for i in range(len(chromosome)-1):
-            trip_distance+=distance(chromosome[i],chromosome[i+1])
-        return trip_distance
-
-    def distance(position1,position2):
-        return pow((pow((map_client(position1)[0]-map_client(position2)[0]),2)+pow((map_client(position1)[1]-map_client(position2)[1]),2)),0.5)
-
-    fitness=cost_per_car*(chromosome.count(0)-1)+TripDistance(chromosome)
-    return fitness
+# tri la population par ordre croissant de la fitness
+# sumpprimer la moins forte chromosome
 
 def verification(chromosome):
     if chromosome :
@@ -175,21 +101,82 @@ def verification(chromosome):
     else:
         return False
 
-
-
-cost_per_car=100
-customer=[]
-map_client=[]
-
-
-def genetic_algorithm_t(Problem_Genetic,k,opt,ngen,size,rate_cross,rate_mutate):
+def genetic_algorithm(Problem_Genetic,target_optimisation,num_generation,population,rate_cross,rate_mutation,num_parent):
 
     def initialize_population(Problem_Genetic,size):
         def generate_chromosome():
             chromosome=[]
-            for i in Problem_Genetic.genes:
+            for i in chromosome_modele:# chromosome_modele: a liste consiste of all of the client number only for once.
                 chromosome.append(i)
+            number_car=11
+            for i in range(random.randint(0,number_car-1)):
+                chromosome.append(0)
             random.shuffle(chromosome)
+            chromosome.append(0)
+            chromosome.insert(0,0)
+
             return chromosome
         return [generate_chromosome() for _ in range(size)]
+    
+    def evolution(Problem_Genetic,num_parent,population,rate_mutation):
+    #   selection      
+    #   crossover
+    #   mutation
+    #   elimination
+        def binary_tournement(Problem_Genetic,population,num_parent):
+            parents=[]            
+            for i in range(num_parent):
+                candidate=random.sample(population,2)
+                if fitness(candidate[0])> fitness(candidate[1]):
+                    parents.append(candidate[0])
+                else:
+                    parents.append(candidate[1])
+            return parents
 
+        def pop_crossover(Problem_Genetic,parents,population):
+            for i in len(parents):#生四个
+                parent=random.sample(parents,2)
+                population.append(Problem_Genetic.crossover(parent[0],parent[1]))
+            return population
+        
+        def pop_mutation(Problem_Genetic,population,rate_mutation):
+            for i in population:
+                i=Problem_Genetic.mutation(i,rate_mutation)
+            return population
+        
+        def eliminate(population): #num_elimination=num_parent: num_pop stable
+            list_fitness=[]
+            for chromosome in population:
+                list_fitness.append(fitness(chromosome))
+            critere=median(list_fitness)
+            for i in population:
+                if fitness(i)<critere:
+                    population.remove(i)
+        
+        def regeneration(population):
+            
+                
+
+
+def distance(client1,client2):
+    return pow((pow((map_client(position1)[0]-map_client(position2)[0]),2)+pow((map_client(position1)[1]-map_client(position2)[1]),2)),0.5)
+
+def TripDistance(chromosome):
+
+    trip_distance =0
+    for i in range(len(chromosome)-1):
+        trip_distance+=distance(chromosome[i],chromosome[i+1])
+    return trip_distance
+
+def fitness(chromosome):
+
+    fitness=cost_per_car*(chromosome.count(0)-1)+TripDistance(chromosome)
+    return fitness
+
+
+    
+
+for g in num_generation:
+
+    #   fitness evaluation
+    #   demande met verification
