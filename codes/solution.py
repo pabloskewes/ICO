fba13f2334f13202607aff3a8e7617650ced8d88
pabloskewes <pabloskewes@gmail.com>
2022-03-17 03:16:52 +0100

    added verbosity to cost function

--- codes/solution.py
+++ codes/solution.py
@@ -65,19 +65,20 @@ def solution_checker(vrptw, sol, verbose=0):
                 return False
     return True
     
-    
-# cost
-# returns the total cost of the solution given for the problem given
-# omega is the weight of each vehicle, 1000 by default
-def cost(vrptw, sol, omega=1000):
+
+def cost(vrptw, solution, omega=1000, verbose=0):
+    """
+    returns the total cost of the solution given for the problem given omega is the weight of each vehicle,
+    1000 by default.
+    """
     # data retrieval
-    nb_vehicle = sol.count(0)-1
+    nb_vehicle = solution.count(0)-1
     distance_matrix = vrptw.distances
     cost_km = vrptw.vehicle.cost_km
     customers = vrptw.customers
     
     # solution given -> list of routes
-    sol_list = sol_to_list_routes(sol)
+    sol_list = sol_to_list_routes(solution)
     
     # sum of the distance of each route
     route_length = 0
@@ -87,8 +88,9 @@ def cost(vrptw, sol, omega=1000):
     
     # total cost calculation
     total_cost = omega*nb_vehicle + cost_km*route_length
-    print('Solution:', sol_list)
-    print('Total cost of solution:', total_cost)
+    if verbose >= 1:
+        print('Solution:', sol_list)
+        print('Total cost of solution:', total_cost)
     return total_cost
 
 
