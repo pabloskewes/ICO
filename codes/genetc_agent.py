c72079d29cfe01d92f1bcfc7d4c41de4ba68cd5e
simpoo2 <46756106+simpoo2@users.noreply.github.com>
2022-03-17 13:38:16 +0100

    Update genetc_agent.py

--- codes/genetc_agent.py
+++ codes/genetc_agent.py
@@ -1,21 +1,50 @@
-class client():
-    def __init__(self,id,weight,volumn,time_window,vehicles,possition,service_time):
-        self.id=id
-        self.weight=weight
-        self.volumn=volumn
-        self.time_window=time_window
-        self.vehicles=vehicles
-        self.possition=possition
-        self.service_time=service_time
+import random
 
-class vehicle():
-    def __init__(self,id,cap_weight,cap_volumn) :
-        self.id=id
-        self.cap_weight=cap_weight
-        self.cap_volumn=cap_volumn
+class Vehicle:
+    def __init__(self, volume, weight, cost_km):
+        self.volume = volume
+        self.weight = weight
+        self.cost_km = cost_km
+        # self.id= id
+
+    def __str__(self):
+        return f'Vehicle of volume {self.volume}, weight {self.weight}'
+
+    
+class Customer:
+    def __init__(self, identifier, latitude, longitude, time_window, request_volume, request_weight, time_service):
+        self.id = identifier
+        # self.code_customer = code_customer
+        self.latitude = latitude
+        self.longitude = longitude
+        self.time_window = time_window
+        self.request_volume = request_volume
+        self.request_weight = request_weight
+        self.time_service = time_service
         
+    def __str__(self):
+        return f'This customer\'s id is {self.id}, its code_customer is {self.code_customer}, ' \
+               f'its latitude is {self.latitude}, its longitude is {self.longitude}, ' \
+               f'its time window is {self.time_window}, its volume requested is {self.request_volume},' \
+               f'its weight requested is {self.request_weight}, its time service is {self.time_service}.'
 
-class modele_genetic():
+class VRP:
+    """
+    Vehicle Routing Problem Time Windows
+    """
+    def __init__(self, customers, distances, time_matrix, vehicle):
+        self.customers = customers
+        self.distances = distances
+        self.time_matrix = time_matrix
+        self.vehicle = vehicle
+        # self.cust_codes = cust_codes
+        
+    def __str__(self):
+        return f'Here are the customers : {self.customers}'
+
+
+
+class Modele_genetic():
     
     def __init__(self,chromosome_modele,len_chromosome):
         self.chromosome_modele=chromosome_modele
@@ -25,8 +54,8 @@ class modele_genetic():
 
         def inversion_mutation(chromosome_aux):#inversion globle
             chromosome=chromosome_aux
-            head=randrange(0,len(chromosome))
-            end=randrange(head,len(chromosome))
+            head=random.randrange(0,len(chromosome))
+            end=random.randrange(head,len(chromosome))
             tmp=chromosome[head:end]
             tmp.reverse()
             return chromosome[:head]+tmp+chromosome[end:] 
@@ -72,7 +101,7 @@ class modele_genetic():
         
         return  process_gen_repeated(child1, child2)
 
-    def distance(self,position1,position2): # Calculate the distance between two positions
+    def distance(self,VRP,position1,position2): # Calculate the distance between two positions
 
         return pow((pow((map_client(position1)[0]-map_client(position2)[0]),2)+pow((map_client(position1)[1]-map_client(position2)[1]),2)),0.5)
 
@@ -83,31 +112,31 @@ class modele_genetic():
             trip_distance+=self.distance(chromosome[i],chromosome[i+1])
         return trip_distance
 
-    def fitness(self,chromosome):# Calculate the fitness of a chromosome, here the fitness is determined by the reciprocal of cost
+    def fitness(self,vrp,chromosome):# Calculate the fitness of a chromosome, here the fitness is determined by the reciprocal of cost
 
-        cost_per_car=1000
-        cost_route=0.2
-        cost=cost_per_car*(chromosome.count(0)-1)+cost_route*self.TripDistance(chromosome)
-        fitness=1/cost
+        cost1=cost(vrp,chromosome)
+        fitness=1/cost1
         return fitness
 
 class VRP_GA():
 
-    def __init__(self,modele_genetic,num_generation,population,rate_mutation,num_parent,num_pop):
+    def __init__(self,modele_genetic,num_generation,population,rate_mutation,num_parent,num_pop,chromosome_modele,vrp):
         self.modele_genetic = modele_genetic
         self.num_generation = num_generation
         self.population = population
         self.rate_mutation = rate_mutation
         self.num_parent = num_parent
         self.num_pop=num_pop
+        self.chromosome_modele = chromosome_modele
+        self.vrp = vrp
 
-    def verification(chromosome):
+    def verification(self,chromosome):
         if chromosome :
             return True
         else:
             return False
 
-    def generate_chromosome():
+    def generate_chromosome(self,chromosome_modele):
         chromosome=[]
         for i in chromosome_modele:# chromosome_modele: a liste consiste of all of the client number only for once.
             chromosome.append(i)
@@ -120,34 +149,34 @@ class VRP_GA():
 
         return chromosome
 
-    def initialize_population(self,num_pop):
+    def initialize_population(self,num_pop,chromosome_modele):
 
-        return [self.generate_chromosome() for _ in range(num_pop)]
+        return [self.generate_chromosome(chromosome_modele) for _ in range(num_pop)]
     
-    def evolution(modele_genetic,num_parent,population,rate_mutation,num_pop): # Realize a generation, including the mating, the mutation, the elimination and the regeneration
+    def evolution(self,modele_genetic,num_parent,population,rate_mutation,num_pop): # Realize a generation, including the mating, the mutation, the elimination and the regeneration
 
-        def binary_tournement(modele_genetic,population,num_parent):# Select certain individuals as parents by their fitness
+        def binary_tournement(self,modele_genetic,population,num_parent):# Select certain individuals as parents by their fitness
             parents=[]            
             for i in range(num_parent):
-                candidate=sample(population,2)
+                candidate=random.sample(population,2)
                 if modele_genetic.fitness(candidate[0])> modele_genetic.fitness(candidate[1]):
                     parents.append(candidate[0])
                 else:
                     parents.append(candidate[1])
             return parents
 
-        def pop_crossover(modele_genetic,parents,population):# Realize mating between parents 
+        def pop_crossover(self,modele_genetic,parents,population):# Realize mating between parents 
             for i in len(parents): 
-                parent=sample(parents,2)
+                parent=random.sample(parents,2)
                 population.append(modele_genetic.crossover(parent[0],parent[1]))
             return population
         
-        def pop_mutation(modele_genetic,population,rate_mutation):# Realize mutation for all members in the population
+        def pop_mutation(self,modele_genetic,population,rate_mutation):# Realize mutation for all members in the population
             for i in population:
                 i=modele_genetic.mutation(i,rate_mutation)
             return population
         
-        def eliminate(modele_genetic,population): # Eliminate the less strong half of the population
+        def eliminate(self,modele_genetic,population): # Eliminate the less strong half of the population
             list_fitness=[]
             for chromosome in population:
                 list_fitness.append(modele_genetic.fitness(chromosome))
@@ -159,6 +188,7 @@ class VRP_GA():
 
         def regeneration(self,population,num_pop): # Generate new-borns to maintain the number of population remains stable
             curr_population=len(population)
+
             for i in num_pop-curr_population:
                 population.append(self.generate_chromosome())
             return population
@@ -168,8 +198,83 @@ class VRP_GA():
         population=pop_mutation(modele_genetic,population,rate_mutation)
         population=eliminate(population)
         population=regeneration(population,num_pop)
-
-
-
+        return population
 
     
+
+def sol_to_list_routes(sol):
+    indexes = [i for i,x in enumerate(sol) if x == 0]
+    liste_divided = [sol[indexes[i]:indexes[i+1]]+[0] for i in range(len(indexes)-1)]
+    return liste_divided
+#[route1,route2,route3,etc], route1=[0,212,2314,4325,43524,4,1,2135,0]
+
+def list_routes_to_sol(sol_list):
+    final_sol = []
+    for sol in sol_list:
+        final_sol += sol[:-1]
+    return final_sol + [0]
+    
+
+def solution_checker(vrptw, sol):
+    nb_cust = len(vrptw.customers) # Number of customers (depot included)
+    # If all customers are not visited, return False
+    if set(sol)!= set(range(nb_cust)):# sol的组成：？[0,1,2,3,4,5,etc] or [0,23145,435456,13244,134565432,etc]?
+        return False
+    # If some nodes (customers) are visited more than once (except for the depot), return False
+    nb_depot = sol.count(0)
+    if len(sol) != nb_depot+nb_cust-1:
+        return False
+    vehicle = vrptw.vehicle
+    volume, weight, cost_km = vehicle.volume, vehicle.weight, vehicle.cost_km 
+    sol_routes = sol_to_list_routes(sol) #list of routes
+    time_matrix = vrptw.time_matrix
+    customers = vrptw.customers
+    print("")
+
+    for route in sol_routes:
+        weight_cust, volume_cust, time_delivery = 0, 0, 0
+        for identifier in route:
+            cust = customers[identifier]
+            print(cust)
+            
+            weight_cust += cust.request_weight
+            volume_cust += cust.request_volume
+            print(weight_cust, volume_cust)
+        # If the weight (or volume) capacity of the vehicle is < to the total weight asked by customers, return False
+        print(weight, volume, weight_cust, volume_cust)
+        if weight < weight_cust or volume < volume_cust :
+            return False
+        for index,identifier in enumerate(route):
+            cust = [customer for customer in customers if customer.id == identifier][0]
+            cust_plus_1 = [customer for customer in customers if customer.id == route[index+1]][0]
+            time_delivery += time_matrix[cust.code_customer,cust_plus_1.code_customer]
+            # If the vehicle gets there befor the beginning of the customer's time window, return False
+            if time_delivery<cust_plus_1.time_window[0]:
+                return False
+            time_delivery += cust_plus_1.time_service
+            # If the end of the delivery is after the end of the customer's time window, return False
+            if time_delivery>cust_plus_1.time_window[1]:
+                return False
+    return True
+
+def cost(vrp,sol, omega=1000):
+    # data retrieval
+    nb_vehicle = sol.count(0)-1
+    distance_matrix = vrp.distances
+    cost_km = vrp.vehicle.cost_km
+    customers = vrp.customers
+    
+    # solution given -> list of routes
+    sol_list = sol_to_list_routes(sol)
+    
+    # sum of the distance of each route
+    route_length = 0
+    for route in sol_list:
+        for i in range(len(route)-1):
+            route_length += distance_matrix[route[i]][route[i+1]]
+    
+    # total cost calculation
+    total_cost = omega*nb_vehicle + cost_km*route_length
+    print('Solution:', sol_list)
+    print('Total cost of solution:', total_cost)
+    return total_cost
