d5f52d4f1724a20630c65bafdf4fb06cffe4bcca
mlenglet <76101249+mlenglet@users.noreply.github.com>
2022-03-27 18:26:18 +0200

    ajout de l'agent recuit simulé dans le fichier tabu_agent.py

--- codes/tabu_agent.py
+++ codes/tabu_agent.py
@@ -1,6 +1,8 @@
 from mesa import Agent, Model
 from tabu import tabu_method
+from recuit_simule import simulated_annealing
 
+# Agent ordonnanceur Tabou
 class TabuAgent(Agent):
     def __init__(self, id_agent, sma):
         super().__init__(id_agent, sma)
@@ -11,6 +13,22 @@ class TabuAgent(Agent):
     
     def step(self, vrptw, sol):
         tabu_method(vrptw, sol)
+
+
+# Agent ordonnanceur Recuit Simulé
+class AgentRS(Agent):
+    #le constructeur
+    def __init__(self,id,model):
+        super().__init__(id,model)
+        self.name = "AgentRS"+str(self.id)
+
+    def __str__(self):
+        return "RS agent : " + self.name
+
+    def step(self, vrptw, sol, T0):
+        simulated_annealing(vrptw, sol, T0, cooling_factor=0.9, max_cycle_iter=100)
+
+
 
 # proposition de modèle
 class SMA(Model):
