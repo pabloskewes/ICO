88e0ee295965979837fcb068659db85efd4237fb
nicoacevedor <nacevedo@dim.uchile.cl>
2022-04-18 22:17:54 +0200

    random solution function fixed

--- codes/flexible_vrptw.py
+++ codes/flexible_vrptw.py
@@ -31,76 +31,82 @@ class FlexVRPTWSolution(VRPTWSolution):
         total_cost += len(self.missing_customers) * self.cust_penalty
         return total_cost
 
-    def customers_checker(self) -> bool:
-        """
-        Checks whether a solution is legitimate regarding the number of visits of customers under the context determined
-        by a flexible vrptw context instance.
-        :return: bool that indicates whether all customers are
-        visited exactly once.
-        """
-        counts = Counter(self.sol_code)
-        del counts[0]
-        return all(count == 1 for count in counts.values())
-
     def checker(self):
-        customers_check = self.customers_checker()
+        not_repeated_customers_check = self.not_repeated_customers_checker()
         route_check = all((self.route_checker(route) for route in self.routes))
-        return customers_check and route_check
+        return not_repeated_customers_check and route_check
 
 
 class FlexVRPTWNeighborhood(VRPTWNeighborhood):
-    @classmethod
-    def random_solution(cls, nb_cust, force_check_vrptw=None, verbose=0) -> FlexVRPTWSolution:
-        tot_customers = len(cls.context.customers) - 1
-        if nb_cust < tot_customers // 2:
-            return cls.random_solution(nb_cust=tot_customers,
-                                       force_check_vrptw=force_check_vrptw,
-                                       verbose=verbose)
-        numbers = list(range(1, tot_customers + 1))
-        random.sample(numbers, nb_cust)
-        proportion = random.choice([0.2, 0.3, 0.4])
-        n_0 = int(nb_cust * proportion)
-        zero_positions = []
-        zero_pos_candidates = list(range(1, nb_cust - 1))
-        for _ in range(n_0):
-            if verbose >= 2:
-                print('candidates:', zero_pos_candidates)
-            try:
-                zero_pos = random.choice(zero_pos_candidates)
-            except IndexError:
-                if verbose >= 1:
-                    print('A problem ocurred, generating new random solution')
-                return cls.random_solution(nb_cust=nb_cust - 1,
-                                           force_check_vrptw=force_check_vrptw,
-                                           verbose=verbose)
-            if verbose >= 2:
-                print('zero_pos chosen:', zero_pos)
-            zero_pos_candidates = list(set(zero_pos_candidates) - {zero_pos, zero_pos + 1, zero_pos - 1})
-            zero_positions.append(zero_pos)
-        for pos in zero_positions:
-            numbers.insert(pos, 0)
-        solution = [0] + numbers + [0]
-        string = str(solution).replace('0, 0, 0', '0').replace('0, 0', '0')
-        code_solution = list(map(int, string.strip('][').split(',')))
-        if code_solution[-1] != 0:
-            code_solution.append(0)
-        solution = FlexVRPTWSolution(code_solution)
-        if force_check_vrptw:
-            check = all((solution.route_checker(route) for route in solution.routes))
-            if not check:
-                if verbose >= 1:
-                    print('Solution generated is not legitimate, a new one will be created.')
-                del numbers, proportion, n_0, zero_positions, zero_pos, zero_pos_candidates
-                del solution, string, code_solution
-                collect()
-                return cls.random_solution(nb_cust=nb_cust - 1,
-                                           force_check_vrptw=force_check_vrptw,
-                                           verbose=verbose)
+    def random_solution(self, nb_cust) -> Solution:
+        """
+        Generates a random pattern of numbers between 0 and nb_cust, in the form of a solution.
+        :param nb_cust: Number of customers wanted in the solution to be generated
+        :return: Solution (or nothing)
+        """
+        
+        def simplify(L, simpleL=[], i=0, on_zero=False):
+            if i >= len(L):
+                return simpleL
+            if L[i] == 0:
+                if on_zero:
+                    return simplify(L, simpleL, i+1, True)
+                else:
+                    return simplify(L, simpleL+[L[i]], i+1, True)
             else:
-                if verbose >= 1:
+                return simplify(L, simpleL+[L[i]], i+1, False)
+                    
+                
+        numbers = list(range(1, nb_cust + 1))
+        is_sol = False
+        n_iter = 0
+        min_length = len(numbers)//2
+        
+        while not is_sol:
+            n_iter += 1
+            if (n_iter % self.max_iter == 0) and len(numbers) > min_length:
+                r = random.randint(0, len(numbers))
+                numbers.remove(numbers[r])
+            
+            random.shuffle(numbers)
+            proportion = random.choice([0.2, 0.3, 0.4])
+            n_0 = int(nb_cust * proportion)
+            zero_positions = []
+            zero_pos_candidates = list(range(1, nb_cust - 1))
+            
+            for _ in range(n_0):
+                if self.verbose >= 2:
+                    print('candidates:', zero_pos_candidates)
+                try:
+                    zero_pos = random.choice(zero_pos_candidates)
+                except IndexError:
+                    if self.verbose >= 1:
+                        print('A problem ocurred, generating new random solution')
+                    continue
+                if self.verbose >= 2:
+                    print('zero_pos chosen:', zero_pos)
+                zero_pos_candidates = list(set(zero_pos_candidates) - {zero_pos, zero_pos + 1, zero_pos - 1})
+                zero_positions.append(zero_pos)
+                
+            for pos in zero_positions:
+                numbers.insert(pos, 0)
+            solution = [0] + numbers + [0]
+            code_solution = simplify(solution)
+            if code_solution[-1] != 0:
+                code_solution.append(0)
+            solution = Sol(code_solution)
+       
+            check = solution.checker()
+            if not check:
+                if self.verbose >= 1:
+                    print('Solution generated is not legitimate, a new one will be created.')
+                continue
+            else:
+                is_sol = True
+                if self.verbose >= 1:
                     print(f'A legitimate solution was successfully generated:\n{solution}')
+                
         return solution
-
 
 class FlexVRPTWSolutionSpace(VRPTWSolutionSpace):
     pass
