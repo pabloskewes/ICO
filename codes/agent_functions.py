2b6cc7767b5a9c6cd2e79740f97fe5a23ecd7e33
nicoacevedor <nacevedo@dim.uchile.cl>
2022-04-21 02:25:10 +0200

    tabu agent created

--- codes/agent_functions.py
+++ codes/agent_functions.py
@@ -14,8 +14,6 @@ class SimulatedAnnealingAgent:
             init_sol = neighborhood.initial_solution()
 
         best_sol = init_sol
-        # self.evolution_best_solution.append(best_sol.cost())
-        # self.evolution_explored_solutions.append(actual_sol.cost())
         neighbor = neighborhood(best_sol)
         dc = neighbor.cost() - best_sol.cost()
         # if the neighbor cool down the system (less entropy)
@@ -27,4 +25,40 @@ class SimulatedAnnealingAgent:
             q = random()
             best_sol = neighbor if q < prob else best_sol
         self.t *= self.cooling_factor
+        return best_sol
+
+
+
+class TabuAgent:
+
+    def __init__(self, max_tabu: int = 10, max_iter: int = 100, tabu_mode: str = 'default'):
+        self.max_tabu = max_tabu
+        self.tabu_mode = tabu_mode
+        self.T = TabuList(mode=self.tabu_mode)
+        self.max_iter = max_iter
+        self.last_solution = None
+
+    def explore(self, neighborhood, init_sol=None):
+        if init_sol is None:
+            init_sol = neighborhood.initial_solution()
+
+        if self.last_solution is None:
+            self.last_solution = init_sol
+
+        self.T.push(init_sol)
+        new_solution = neighborhood(init_sol)
+        n_cycle = 0
+        while self.T.contains(new_solution):
+            new_solution = neighborhood(init_sol)
+            if n_cycle == self.max_iter:
+                new_solution = self.last_solution
+                break
+            n_cycle += 1
+        if new_solution.cost() < self.last_solution.cost():
+            self.last_solution = new_solution
+
+        return new_solution
+
+
+
         return best_sol
