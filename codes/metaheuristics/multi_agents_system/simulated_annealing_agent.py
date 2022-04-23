fe440a342c279bcca77d96342e33d94fe08513c0
nicoacevedor <nacevedo@dim.uchile.cl>
2022-04-23 04:57:42 +0200

    SA agent upgraded

--- codes/metaheuristics/multi_agents_system/simulated_annealing_agent.py
+++ codes/metaheuristics/multi_agents_system/simulated_annealing_agent.py
@@ -1,25 +1,50 @@
 from random import random
+from numpy import exp
 
 
-class SimulatedAnnealingAgent:
-    def __init__(self, t0: int = 30, cooling_factor: float = 0.9):
-        self.t = t0
+class SAAgent:
+    def __init__(self, t0: int = 30, cooling_factor: float = 0.9, max_iter=100, init_sol=None):
+        self.t0 = t0
+        self.t = self.t0
         self.cooling_factor = cooling_factor
+        self.max_iter = max_iter
+        self.n_iter = 0
+        self.is_finished = False
+        self.init_sol = init_sol
+        self.actual_sol = self.init_sol
+        self.best_sol = self.init_sol
+        self.new_cycle = False
 
-    def explore(self, neighborhood, init_sol=None):
-        if init_sol is None:
-            init_sol = neighborhood.initial_solution()
+    def reset_params(self):
+        self.t = self.t0
+        self.is_finished = False
 
-        best_sol = init_sol
-        neighbor = neighborhood(best_sol)
-        dc = neighbor.cost() - best_sol.cost()
-        # if the neighbor cool down the system (less entropy)
-        # we update the best_solution
-        best_sol = neighbor if dc < 0 else best_sol
-        # if not we calculate the probability
-        if dc > 0:
-            prob = np.exp(-1.0* dc / self.t)
-            q = random()
-            best_sol = neighbor if q < prob else best_sol
-        self.t *= self.cooling_factor
-        return best_sol
+    def explore(self, neighborhood):
+        if not self.is_finished:
+            self.n_iter += 1
+            neighbor = neighborhood(self.actual_sol)
+            dc = neighbor.cost() - self.best_sol.cost()
+            # if the neighbor cool down the system (less entropy)
+            # we update the best_solution
+            if dc < 0:
+                self.actual_sol = neighbor
+                self.new_cycle = True
+            # if not we calculate the probability
+            if dc > 0:
+                prob = exp(-1.0* dc / self.t)
+                q = random()
+                if q < prob:
+                    self.actual_sol = neighbor
+                    self.new_cycle = True
+
+            if self.actual_sol.cost() < self.best_sol.cost():
+                self.best_sol = self.actual_sol
+
+            if self.n_iter >= self.max_iter:
+                self.n_iter = 0
+                self.t *= self.cooling_factor
+                if not self.new_cycle:
+                    self.is_finished = True
+                self.new_cycle = False
+
+        return self.best_sol
