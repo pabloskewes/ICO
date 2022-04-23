627015ea52f68a6bd41b58bd2d159c59e859c872
nicoacevedor <nacevedo@dim.uchile.cl>
2022-04-23 04:57:03 +0200

    explore function created

--- codes/metaheuristics/multi_agents_system/base_agent.py
+++ codes/metaheuristics/multi_agents_system/base_agent.py
@@ -5,6 +5,13 @@ class BaseAgent(MesaAgent):
     def __init__(self, id, model):
         super().__init__(id, model)
         self.id = id
+        self.neighborhood = model.neighborhood
+        self.solution = None
 
     def step(self):
         pass
+
+    def explore(self):
+        N = self.neighborhood()
+        self.solution = N(self.solution)
+        return self.solution
