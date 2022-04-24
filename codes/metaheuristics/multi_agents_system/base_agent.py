033803cda0ad3c0babbc28ab5351e53e82b0c4e1
nicoacevedor <nacevedo@dim.uchile.cl>
2022-04-24 18:11:58 +0200

    BaseAgent push function added

--- codes/metaheuristics/multi_agents_system/base_agent.py
+++ codes/metaheuristics/multi_agents_system/base_agent.py
@@ -1,25 +1,34 @@
 from .sequencial_models import SequentialModel
 from ..base_problem import Solution
+from .pools import BasePool
 
 from mesa import Agent as MesaAgent
 
 
 class BaseAgent(MesaAgent):
-    def __init__(self, unique_id: int, model: SequentialModel):
+    def __init__(self, unique_id: int, model: SequentialModel,
+                 push_to: List[BasePool] = None, pull_from: List[BasePool] = None):
         super().__init__(unique_id, model)
         self.unique_id = unique_id
         self.problem = model.problem
         self.pools = model.pools
         self.N = self.problem.NEIGHBORHOOD()
+        self.push_to = push_to
+        self.pull_from = pull_from
         self.in_solution = None
         self.out_solution = None
 
     def step(self):
         pass
 
+    def push(self) -> None:
+        if self.push_to is None:
+            return
+        for pool in self.push_to:
+            pool.push(self.out_solution)
+
     def explore(self) -> Solution:
         """ Basic explore that simply looks for a neighborhood with the default configuration """
         N = self.N
         self.out_solution = N(self.in_solution)
         return self.out_solution
-
