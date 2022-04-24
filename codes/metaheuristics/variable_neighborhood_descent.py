f66dc2c7ca1d68f809293a73feb7a8687e5c4112
nicoacevedor <nacevedo@dim.uchile.cl>
2022-04-24 14:14:36 +0200

    VND algorithm created

---
+++ codes/metaheuristics/variable_neighborhood_descent.py
@@ -0,0 +1,39 @@
+from .base_metaheuristic import BaseMetaheuristic
+from .base_problem import Solution
+
+
+class VariableNeighborhoodDescent(BaseMetaheuristic):
+
+    def __init__(self, solution_params=None, neighborhood_params=None, solution_space_params=None):
+        super().__init__()
+
+        self.params = {'solution': solution_params,
+                       'neighborhood': neighborhood_params,
+                       'solution_space': solution_space_params}
+
+    def search(self) -> Solution:
+        """
+        Returns the best solution applying the Tabu Search algorithm
+        :return: Solution
+        """
+        # Initialization of parameters
+        _, N, _ = self.get_problem_components()
+        solution = N.initial_solution()
+        k_neighborhood = 1
+        k_max = len(N.use_methods)
+        while k_neighborhood <= k_max:
+            N.set_params({'choose_mode': k_neighborhood})
+            # We look for a neighbor
+            new_solution = N.get_neighbor(solution)
+            self.evolution_best_solution.append(solution.cost())
+            self.evolution_explored_solutions.append(new_solution.cost())
+            # If the neighbor is better than our previous solution,
+            # the solution is updated and the process starts again.
+            # If not, we look into the next neighborhood
+            if new_solution.cost() < solution.cost():
+                solution = new_solution
+                k_neighborhood = 1
+            else:
+                k_neighborhood += 1
+
+        return solution
