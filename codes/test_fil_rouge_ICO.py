9267275c39aae367c56af37effd5b3dc5a28701e
pabloskewes <pabloskewes@gmail.com>
2022-04-28 06:51:30 +0200

    aa

--- codes/test_fil_rouge_ICO.py
+++ codes/test_fil_rouge_ICO.py
@@ -1,19 +1,60 @@
-from FilRouge.ICO.codes.loading_models import load_solomon
-from vrptw import VRPTW
-from FilRouge.ICO.codes.solution import VRPTWSolution as Sol
-
+import threading
+import multiprocessing as mp
+import time
+import random
 from metaheuristics.simulated_annealing import SimulatedAnnealing
+from loading_models import load_solomon
+from vrptw import VRPTW
 
-vrptw_context = load_solomon('simple.csv', nb_cust=10, vehicle_speed=100)
-vrptw = VRPTW(vrptw_context)
-init_sol = Sol([[0, 7, 5, 0], [0, 6, 0], [0, 8, 2, 0], [0, 3, 4, 9, 10, 1, 0]])
-rs = SimulatedAnnealing(t0=30, neighborhood_params={'verbose': 0, 'init_sol': 'random'})
 
-# print('init sol:')
-# print(init_sol)
-# print('cost =', init_sol.cost())
-sol = rs.fit_search(vrptw)
-# print('sol found:')
-# print(sol)
-# print('cost =', sol.cost())
-rs.plot_evolution_best_sol()
+vrptw_data = load_solomon('A50.csv', nb_cust=None, vehicle_speed=100)
+vrptw = VRPTW(vrptw_data)
+
+neighborhood_params = {'verbose': 0,
+                       'init_sol': 'random',
+                       'choose_mode': 'random',
+                       'use_methods': [1, 2, 3, 4, 5, 6, 7, 8],
+                       'force_new_sol': True,
+                       'full_search': True}
+
+# rs = SimulatedAnnealing(t0=30, progress_bar=True, neighborhood_params=neighborhood_params)
+# rs_sol = rs.fit_search(vrptw)
+# rs.plot_evolution_cost(figsize=(17,5))
+
+print("Number of processors: ", mp.cpu_count())
+
+# # Python Threading Example for Beginners
+# # First Method
+# def greet_them(people):
+#     for person in people:
+#         print("Hello Dear " + person + ". How are you?")
+#         time.sleep(0.5)
+#
+#
+# # Second Method
+# def assign_id(people):
+#     i = 1
+#     for person in people:
+#         print("Hey! {}, your id is {}.".format(person, i))
+#         i += 1
+#         time.sleep(0.5)
+#
+#
+# people = ['Richard', 'Dinesh', 'Elrich', 'Gilfoyle', 'Gevin']
+#
+# t = time.time()
+#
+# # Created the Threads
+# t1 = threading.Thread(target=greet_them, args=(people,))
+# t2 = threading.Thread(target=assign_id, args=(people,))
+#
+# # Started the threads
+# t1.start()
+# t2.start()
+#
+# # Joined the threads
+# t1.join()
+# t2.join()
+#
+# print("Woaahh!! My work is finished..")
+# print("I took " + str(time.time() - t))
