aaff1bbac211817e3cdc7c6a72e6f11cba3220f0
pabloskewes <pabloskewes@gmail.com>
2022-04-29 22:56:37 +0200

    added models

---
+++ codes/metaheuristics/models.py
@@ -0,0 +1,14 @@
+from .simulated_annealing import SimulatedAnnealing as SA
+from .tabu_search import TabuSearch as TS
+from .genetic_algorithm import GeneticAlgorithm as GA
+from .greedy_search import GreedySearch as GS
+from .variable_neighborhood_descent import VariableNeighborhoodDescent as VND
+from .multi_agents_system.multi_agent_system import MultiAgentSystem as MAS
+
+
+SimulatedAnnealing = SA
+TabuSearch = TS
+GeneticAlgorithm = GA
+GreedySearch = GS
+VariableNeighborhoodDescent = VND
+MultiAgentSystem = MAS
